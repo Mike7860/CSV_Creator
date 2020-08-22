7b32bd229e94b10abef5da43f036c7c6413e21f2
micha <michal.78.60@gmail.com>
2020-08-22 20:43:44 +0200

    modules and basic values definition

--- csv_creator.py
+++ csv_creator.py
@@ -1,2 +1,38 @@
-print("CSV creator")
-print("CSV creator2")
+import os
+import openpyxl
+import PyPDF2
+import csv
+import textwrap
+import logging
+import time
+import datetime
+
+from datetime import date
+from openpyxl.workbook import Workbook
+
+curr_dir = os.getcwd()
+
+print('You are in "' +curr_dir+ '" Do you want change directory? y/n')
+
+dir_choise = input()
+
+if dir_choise =='y':
+    print('Please provide new directory with necessary files \n>>')
+    os.chdir(input())
+if dir_choise =='n':
+    curr_dir = curr_dir
+
+not_tested = 0
+date = date.today()
+print(date)
+
+class Test:
+    def __init__(self, id, source, flag, auto):
+        self.ID = id
+        self.source = source
+        self.flag = flag
+        self.auto = auto
+
+dict_test_all = {}
+dict_test_basic = {}
+dict_test_extended = {}
